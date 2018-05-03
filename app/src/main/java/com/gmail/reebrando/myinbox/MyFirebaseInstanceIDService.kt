e50e077e769871d1125ae916bd339aed9c4a4b97
Renan Brando <reebrando@gmail.com>
2018-05-03 00:26:40 -0300

    Added push notifications support with notification incoming from intent.

---
+++ app/src/main/java/com/gmail/reebrando/myinbox/MyFirebaseInstanceIDService.kt
@@ -0,0 +1,17 @@
+package com.gmail.reebrando.myinbox
+
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.FirebaseInstanceIdService
+
+
+class MyFirebaseInstanceIDService : FirebaseInstanceIdService() {
+    private var TAG = "MyFirebaseInstanceIDService"
+
+    override fun onTokenRefresh() {
+        //Get updated token
+        var refreshedToken = FirebaseInstanceId.getInstance().token
+        Log.d(TAG,"New Token : "+refreshedToken)
+
+    }
+}
