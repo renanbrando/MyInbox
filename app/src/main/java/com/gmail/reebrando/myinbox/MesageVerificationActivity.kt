db7ab0ac118581aa2d8904b8cac0e1766cdf4f14
arielmarinho <arielmarinho1@gmail.com>
2018-05-08 22:45:41 -0300

    layout tela de verificar mensagem

---
+++ app/src/main/java/com/gmail/reebrando/myinbox/MesageVerificationActivity.kt
@@ -0,0 +1,12 @@
+package com.gmail.reebrando.myinbox
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MesageVerificationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mesage_verification)
+    }
+}
